159c7cb9de960e7c9b092a3a991747ba0054a22d
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-09-27 21:13:22 +0200

    Trying functional programming in login command

--- commands/user/login/login.go
+++ commands/user/login/login.go
@@ -18,15 +18,56 @@ type Response struct {
 	SessionToken string
 }
 
-func (c Command) Execute(req Request) (Response, error) {
-	res := Response{}
-	u, err := c.UserSource.Get(user.Id(req.Id))
-	if nil != err {
-		return res, err
-	}
-	u2, err2 := c.Login.Try(u, user.Passkey(req.Passkey))
-	c.UserSource.Persist(u2)
-	res.SessionToken = string(u2.Token().Code)
+func (c Command) Execute(req Request) (res Response, err error) {
+	tokenCode, err := c.getTokenCodeFromUserIfCorrectLogin(
+		user.Id(req.Id),
+		user.Passkey(req.Passkey),
+	)
+	res.SessionToken = string(tokenCode)
 
-	return res, err2
+	return res, err
+}
+
+func (c Command) getTokenCodeFromUserIfCorrectLogin(uid user.Id, up user.Passkey) (tc user.TokenCode, err error) {
+	u, err := c.getUserIfCorrectLogin(uid, up)
+	if nil != err {
+		return
+	}
+ 	tc = u.Token().Code
+
+	return
+}
+
+func (c Command) getUserIfCorrectLogin(uid user.Id, up user.Passkey) (u user.User, err error) {
+	u, err = c.tryLogin(uid, up)
+	if(nil != err) {
+		return
+	}
+	u, err = c.persistUser(u)
+
+	return
+}
+
+
+func (c Command) persistUser(u user.User) (user.User, error) {
+	c.UserSource.Persist(u)
+	return u, nil
+}
+
+func (c Command) tryLogin(uid user.Id, p user.Passkey) (u user.User, err error) {
+	u, err = c.getUser(uid)
+	if(nil != err) {
+		return
+	}
+	u, err = c.Login.Try(u, p)
+
+	return
+}
+
+func (c Command) getUser(uid user.Id) (user.User, error) {
+	return c.UserSource.Get(uid)
+}
+
+func (c Command) getTokenCodeFromUser(u user.User) user.TokenCode {
+	return u.Token().Code
 }
