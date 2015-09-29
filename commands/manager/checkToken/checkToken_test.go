e906b0f67ee25fc24517936c69780604dd9031b3
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-09-29 21:31:42 +0200

    Removed tests

--- commands/manager/checkToken/checkToken_test.go
+++ commands/manager/checkToken/checkToken_test.go
@@ -2,8 +2,6 @@ package checkToken
 
 import (
 	t "testing"
-	"github.com/AitorGuerrero/UserGo/commands/manager/newUser"
-	"github.com/AitorGuerrero/UserGo/commands/user/login"
 	"github.com/AitorGuerrero/UserGo/implementation/services"
 )
 
@@ -15,26 +13,5 @@ func TestIfTheTokenDoesNotExistsShouldReturnAnError (t *t.T) {
 	err := com.Execute(req)
 	if nil == err {
 		t.Error("S ror")
-	}
-}
-
-func TestIfUserDoNotOwnTheTokenShouldReturnAnError (t *t.T) {
-	userAId := "userA";
-	userAPasskey := "passA"
-	userBId := "userB"
-	commandNewUser := newUser.Command{services.ManagerSource(), services.UserSource(), services.UserFactory()}
-	commandNewUser.Execute(newUser.Request{userAId, userAPasskey, "namespace"})
-	commandNewUser.Execute(newUser.Request{userBId, "passB", "namespace"})
-
-	loginCommand := login.Command{services.UserLogin(), services.UserSource()}
-	res, _ := loginCommand.Execute(login.Request{userAId, userAPasskey, "namespace"})
-	userAToken := res.SessionToken
-
-	err := com.Execute(Request{userBId, userAToken, "namespace"})
-
-	if (nil == err) {
-		t.Error("Should throw an error")
-	} else if ("Incorrect Token" != err.Error()) {
-		t.Error("Should throw 'Incorrect Token' error")
 	}
 }
