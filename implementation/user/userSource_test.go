ffe560890ff3bfd789d0db165f03730ac634bd7f
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-10-04 23:30:26 +0200

    User Source tests extracted for reuse in implementations

--- implementation/user/userSource_test.go
+++ implementation/user/userSource_test.go
@@ -6,48 +6,11 @@ import (
 	t "testing"
 )
 
-var e Encryptor
-var pe user.PasskeyEncryptor
-var i = user.ParseId("de1ec52a-21c6-491c-99f9-6f29bc1d565f")
-var p user.Passkey
-var u user.User
-var s Source
+var ut = user.UserSourceTests{func() user.Source {
+	s := NewSource()
+	return &s
+}}
 
-func beforeEach() {
-	e = Encryptor{}
-	pe = user.PasskeyEncryptor{e}
-	i = user.ParseId("de1ec52a-21c6-491c-99f9-6f29bc1d565f")
-	p = pe.Encrypt(i, "passkey")
-	u = user.New(i, p)
-	s = NewSource()
-}
-
-func TestWhenAddingAUserIfIdExistsShouldReturnError(t *t.T) {
-	beforeEach()
-	u2 := user.New(i, p)
-	s.Add(&u)
-	err := s.Add(&u2)
-	if _, ok := err.(user.DuplicatedIdError); !ok {
-		t.Error(err)
-	}
-}
-
-func TestGettingAUserIfNotExistsShouldReturnAError (t *t.T) {
-	i := user.ParseId("8809a0cd-fd8c-4796-bb06-62d1134962f0")
-	_, err := s.Get(i)
-	if _, ok := err.(user.NotExistentUser); !ok {
-		t.Error(err)
-	}
-}
-
-func TestShouldReturnTheSameUserAsAdded (t *t.T) {
-	s.Add(&u)
-	ru, _ := s.Get(i)
-
-	if !u.Id().Equal(ru.Id()) {
-		t.Error("Id should be the same")
-	}
-	if !u.Token.IsSame(ru.Token) {
-		t.Error("Token should be the same")
-	}
+func TestUserSource(t *t.T) {
+	ut.Run(t)
 }
---
+++ user/UserSourcetest.go
@@ -0,0 +1,68 @@
+package user
+
+import (
+	t "testing"
+)
+
+type fakeEncryptor struct {}
+
+func (fakeEncryptor) Encrypt(i string) string {
+	return i
+}
+
+var e fakeEncryptor
+var pe PasskeyEncryptor
+var i = ParseId("de1ec52a-21c6-491c-99f9-6f29bc1d565f")
+var p Passkey
+var u User
+var s Source
+
+type UserSourceTests struct {
+	Source func() Source
+}
+
+func (ut UserSourceTests) Run(t *t.T) {
+	ut.TestWhenAddingAUserIfIdExistsShouldReturnError(t)
+	ut.TestGettingAUserIfNotExistsShouldReturnAError(t)
+	ut.TestShouldReturnTheSameUserAsAdded(t)
+}
+
+func (ust UserSourceTests) beforeEach() {
+	s = ust.Source()
+	e = fakeEncryptor{}
+	pe = PasskeyEncryptor{e}
+	p = pe.Encrypt(i, "passkey")
+	u = New(i, p)
+}
+
+func (ust UserSourceTests) TestWhenAddingAUserIfIdExistsShouldReturnError(t *t.T) {
+	ust.beforeEach()
+	u2 := New(i, p)
+	s.Add(&u)
+	err := s.Add(&u2)
+	if _, ok := err.(DuplicatedIdError); !ok {
+		t.Error(err)
+	}
+}
+
+func (ust UserSourceTests) TestGettingAUserIfNotExistsShouldReturnAError (t *t.T) {
+	ust.beforeEach()
+	i := ParseId("8809a0cd-fd8c-4796-bb06-62d1134962f0")
+	_, err := s.Get(i)
+	if _, ok := err.(NotExistentUser); !ok {
+		t.Error(err)
+	}
+}
+
+func (ust UserSourceTests) TestShouldReturnTheSameUserAsAdded (t *t.T) {
+	ust.beforeEach()
+	s.Add(&u)
+	ru, _ := s.Get(i)
+
+	if !u.Id().Equal(ru.Id()) {
+		t.Error("Id should be the same")
+	}
+	if !u.Token.IsSame(ru.Token) {
+		t.Error("Token should be the same")
+	}
+}
