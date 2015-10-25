1253e4ff318acbd9e776c4fff42de62089ff2a8a
Aitor Guerrero <aguerrerolasarte@gmail.com>
2015-10-25 10:46:46 +0100

    Services restored

---
+++ implementation/services/domainServices.go
@@ -0,0 +1,27 @@
+package services
+
+import (
+	"github.com/AitorGuerrero/UserGo/user"
+	implementationUser "github.com/AitorGuerrero/UserGo/implementation/user"
+)
+var encrypter = implementationUser.Encryptor{}
+var userPassKeyEncryptor = user.PasskeyEncryptor{encrypter}
+var userFactory = user.Factory{userPassKeyEncryptor}
+var signInValidator = user.SignInValidator{userPassKeyEncryptor}
+var login = user.Login{signInValidator, &userSource}
+
+func UserFactory() user.Factory {
+	return userFactory
+}
+
+func UserPassKeyEncryptor() user.PasskeyEncryptor {
+	return userPassKeyEncryptor
+}
+
+func SignInValidator() user.SignInValidator {
+	return signInValidator
+}
+
+func UserLogin() user.Login {
+	return login
+}
